3bd5ccfe685250e1a062da776fccef4411b5eeb5
enalisnick <enalisnick@gmail.com>
2015-09-02 09:31:59 -0700

    Created script to find nearest neighbors to a word
    Wrote this on the fly.  Not tested (or even run)

---
+++ iWord2Vec/find_nearest_neighbors.py
@@ -0,0 +1,39 @@
+from scipy import spatial
+from tabulate import tabulate
+
+### READ EMBEDDINGS FROM TXT FILE
+embedding_fileName = "output.txt"
+embeddings = []
+vocab = []
+with open(embedding_fileName) as f:
+  for line in f.readlines()[1:]:
+    line = line.strip().split()
+    vocab.append(line[0])
+    embeddings.append([float(x) for x in line[1:]])
+vocab_size = len(vocab)
+
+headers = ["Word", "Cosine Similarity"]
+k = 50 # number of nearest neighbors
+input = "none"
+while True:
+  input = raw_input("Enter word or 'EXIT' to quit: ").split()[0]
+  if input=="EXIT":
+    break
+  word_idx = -1
+  try:
+    word_idx = vocab.index(input)
+    ### compute cosine similarity with all words
+    sims = [-1.]*vocab_size
+    for idx in xrange(vocab_size):
+      if idx != word_idx:
+        sims[idx] = 1 - spatial.distance.cosine(embeddings[word_idx], embeddings[idx])
+    ### get top k most similar 
+    top_k_idxs = sorted(range(vocab_size), key=sims.__getitem__, reverse=True)[:k]
+    t = []
+    print "%d nearest neighbors to %s:" %(k, input)
+    for idx in top_k_idxs:
+      t.append([vocab[idx], sims[idx]])
+    # output table
+    print tabulate(t,headers=headers)
+    except ValueError:
+      print "There's no embedding for that word.  Try again."
