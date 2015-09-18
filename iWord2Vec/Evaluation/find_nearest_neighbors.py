4361675c3802e3dfd69f889605bf1efbd0b3127c
enalisnick <enalisnick@gmail.com>
2015-09-17 23:45:03 -0700

    added dynamic dim sim; not sure if it really works

--- iWord2Vec/Evaluation/find_nearest_neighbors.py
+++ iWord2Vec/Evaluation/find_nearest_neighbors.py
@@ -1,4 +1,5 @@
 import sys
+from math import exp
 import numpy as np
 from scipy import spatial
 from tabulate import tabulate
@@ -28,8 +29,8 @@ if __name__ == '__main__':
       vocab.append(line[0])
       embeddings.append([float(x) for x in line[1:]])
   vocab_size = len(vocab)
-
-  headers = ["Rank", "Word", "Cosine Similarity"]
+  z_max = len(embeddings[0])
+  headers = ["Rank", "Word", "Cosine Similarity", "Num. of Dimensions Used"]
   input = "none"
   while True:
     input = raw_input("Enter word or 'EXIT' to quit: ").split()[0]
@@ -40,26 +41,35 @@ if __name__ == '__main__':
       word_idx = vocab.index(input)
       ### compute cosine similarity with all words
       sims = [-1.]*vocab_size
+      z_vals = [0]*vocab_size
       for idx in xrange(vocab_size):
         if idx != word_idx:
           word_embedding = embeddings[word_idx]
           other_word_embedding = embeddings[idx]
-          if num_dims > 0 and num_dims <= len(embeddings[word_idx]):
-            word_embedding = word_embedding[0:num_dims]
-            other_word_embedding = other_word_embedding[0:num_dims]  
-              
-          sims[idx] = 1 - spatial.distance.cosine(word_embedding, other_word_embedding)
-
+          ### compute how many dimensions to use
+          z = 50
+          max_prob = 0.0
+          running_total = 0.
+          for w,c in zip(word_embedding[:z], other_word_embedding[:z]):
+            running_total += w*c 
+          for z_idx in xrange(z,z_max):
+            running_total += word_embedding[z_idx]*other_word_embedding[z_idx]
+            if exp(running_total) > max_prob:
+              z = z_idx+1
+              max_prob = exp(running_total)
+          z = z_max
+          sims[idx] = 1 - spatial.distance.cosine(word_embedding[:z], other_word_embedding[:z])
+          z_vals[idx] = z
       ### get top k most similar 
       top_k_idxs = sorted(range(vocab_size), key=sims.__getitem__, reverse=True)[:k]
       t = []
       print "%d nearest neighbors to %s:" %(k, input)
       for i, idx in enumerate(top_k_idxs):
-        t.append([i+1, vocab[idx], sims[idx]])
+        t.append([i+1, vocab[idx], sims[idx], z_vals[idx]])
       # output table
       print tabulate(t,headers=headers)
-      print(embeddings[word_idx])
-      print("#non-zero dims: %d" % (np.count_nonzero(embeddings[word_idx])))
+      #print(embeddings[word_idx])
+      #print("#non-zero dims: %d" % (np.count_nonzero(embeddings[word_idx])))
     except ValueError:
       print "There's no embedding for that word.  Try again."
 
