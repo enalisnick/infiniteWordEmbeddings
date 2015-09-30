f752fdeaeedc489983bec5e936ba1c6176bd0a83
Sachin Ravi <sachinravi14@gmail.com>
2015-09-30 14:06:12 -0400

    cleaned up t-sne code

--- iWord2Vec/Visualization/nbhr_tsne.py
+++ iWord2Vec/Visualization/nbhr_tsne.py
@@ -1,33 +1,34 @@
-from Evaluation.eval_lib import read_embedding_file  
-from Evaluation.eval_lib import get_nn
-
 import sys
 import numpy as np
 from sklearn.manifold import TSNE 
 import matplotlib as mpl
-mpl.use('Agg')
+mpl.use('Agg') # so that we can save plot without displaying
 import matplotlib.pyplot as plt
+
+from Evaluation.eval_lib import read_embedding_file  
+from Evaluation.eval_lib import get_nn
 
 if __name__ == '__main__':
   embedding_filename = sys.argv[1]
   word = sys.argv[2]
   K = int(sys.argv[3])
-  num_dims = int(sys.argv[4])
+  num_dims = int(sys.argv[4]) # number of dimensions to do search over
+  path = sys.argv[5] # path where to write plot
+  
   vocab, embeddings = read_embedding_file(embedding_filename)
   dim = len(embeddings[0])
-  path = sys.argv[5] 
- 
   sims, z_vals, top_k_idxs = get_nn(vocab, embeddings, word, K, num_dims)
-  
+
+  ### perform t-SNE   
   X= np.zeros(shape=(K+1, dim))
   X[0,] = embeddings[vocab.index(word)]
   for i, idx in enumerate(top_k_idxs):
-    #print i, idx 
     X[i+1,] = embeddings[idx]
 
   model = TSNE(n_components=2, random_state=0)
   Y = model.fit_transform(X)
-  
+
+  ### set up plot with title and label for points 
   plt.figure(figsize=(15,15))
   plt.scatter(Y[:,0], Y[:,1])
   plt.title("Nearest neighbors for '" + word + "' restricted to " + str(num_dims) + " dims")
