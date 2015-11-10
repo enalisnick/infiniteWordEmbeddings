ca5137b6d1f6a29c1947a5a27e018c93c4b251ed
enalisnick <enalisnick@gmail.com>
2015-11-09 17:20:36 -0800

    increased threshold for printing progress

--- iWord2Vec/iW2V_mod.c
+++ iWord2Vec/iW2V_mod.c
@@ -588,7 +588,7 @@ void *TrainModelThread(void *arg) {
 
   while (1) {
     // track training progress
-    if (word_count - last_word_count > 10000) {
+    if (word_count - last_word_count > 250000) {
       long long diff = word_count - last_word_count;
       word_count_actual += word_count - last_word_count;
       last_word_count = word_count;
