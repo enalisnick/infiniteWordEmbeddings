861f8d6e35c133e38fe6f0c6eb32efe1fe5d133a
Sachin Ravi <sachinravi14@gmail.com>
2015-11-13 22:38:44 -0500

    cleaned up graphing code

--- iWord2Vec/Visualization/graph_p_z.py
+++ iWord2Vec/Visualization/graph_p_z.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -28,76 +29,81 @@ def compute_p_z_given_w(input_embedding, context_embeddings):
         p_z_given_w += compute_unnorm_z_probs_recursively(input_embedding, context_vec, d)
     return p_z_given_w / p_z_given_w.sum()
 
-def get_nearest_neighbors(input_embedding, in_word_idx, context_embeddings, z, k):
-    sparsity_weight = 0.001
-    dim_penalty = 1.1
-    input_embedding = np.array(input_embedding[:z])
-    scores = np.zeros(len(context_embeddings))
-    for idx, context_embed in enumerate(context_embeddings):
-        context_embed = np.array(context_embed[:z])
-        scores[idx] = np.dot(input_embedding, np.array(context_embed[:z]))
+def get_nearest_neighbors(word_embedding, in_word_idx, input_embeddings, context_embeddings, z, k):
+    word_embedding = np.array(word_embedding[:z])
+    scores = np.zeros(len(input_embeddings))
+    for idx, input_embedding in enumerate(input_embeddings):
+        input_embedding = np.array(input_embedding[:z])
+        scores[idx] = np.dot(word_embedding, np.array(input_embedding[:z]))
     scores[in_word_idx] = -100000
     return np.argsort(-scores)[:k]
 
+def graph_p_z_w(p_z_w, word, in_word_idx, word_embedding, input_embeddings, vocab): 
+  num_of_modes_to_plot = 5
+  d = len(word_embedding)
+  num_of_nns_to_get = 5
+
+  # find nearest neighbors at the modes
+  sorted_prob_idx = np.argsort(-1*p_z_w) # negative one so the sort is descending
+  nns_at_modes = []
+  modes_used = []
+  idx = 0
+  while num_of_modes_to_plot > 0:
+      current_idx = sorted_prob_idx[idx]
+      # check if this idx is too close to previous ones
+      mode_flag = False
+      if (current_idx==0 and p_z_w[current_idx]>p_z_w[current_idx+1]) or (current_idx==d-1 and p_z_w[current_idx]>p_z_w[current_idx-1]) or (p_z_w[current_idx]>p_z_w[current_idx-1] and p_z_w[current_idx]>p_z_w[current_idx+1]): 
+	  mode_flag = True
+	  for mode in modes_used:
+	      if abs(mode[0]-current_idx) <= 5:
+		  mode_flag = False
+      if mode_flag:
+	  # get nearest neighbors at current idx
+	  modes_used.append((current_idx, p_z_w[current_idx]))
+	  nns_at_modes.append([vocab[j] for j in get_nearest_neighbors(word_in_embedding, in_word_idx, input_embeddings, context_embeddings, current_idx+1, num_of_nns_to_get).tolist()]) 
+	  num_of_modes_to_plot -= 1
+      idx += 1
+      if idx >= d:
+	  break
+      
+  # plotting the distribution
+  plt.plot()
+  plt.bar([x+1 for x in range(d)], p_z_w, width=1.0, facecolor='blue', edgecolor="blue")
+  
+  # plot the nearest neighbors at the modes
+  for mode_loc, mode_nns in zip(modes_used, nns_at_modes):
+      plt.annotate(', '.join(mode_nns), xy=(mode_loc[0]+1, mode_loc[1]+0.001),  xycoords='data',
+	      xytext=(mode_loc[0]+5, mode_loc[1]+0.005), arrowprops=dict(facecolor='black', shrink=0.05, frac=0.1, headwidth=2, width=1))
+  plt.title("p(z|w="+vocab[in_word_idx]+")")
+  plt.xlim(1,d)
+  plt.ylim(0,modes_used[0][1]+0.007)
+ 
+  return plt 
+
 if __name__ == '__main__':
-    # some hardcoded var, make as input args later
-    input_embedding_file = "/Users/enalisnick/Dropbox/iW2V-models/text8_output_single_thread_grad_fix.txt"
-    context_embedding_file = "/Users/enalisnick/Dropbox/iW2V-models/text8_context_output_single_thread_grad_fix.txt"
     k = 10000
-    word_to_plot = "apple"
     num_of_modes_to_plot = 5
     num_of_nns_to_get = 5
-
+    
+    input_embedding_file = sys.argv[1]  
+    context_embedding_file = sys.argv[2] 
+    word_to_plot = sys.argv[3]
+    
     print "loading embeddings and vocabulary..."
     in_vocab, in_embeddings = read_embedding_file(input_embedding_file)
     in_vocab = in_vocab[:k]
     in_embeddings = in_embeddings[:k]
-    out_vocab, out_embeddings = read_embedding_file(context_embedding_file)
-    out_vocab = out_vocab[:k]
-    out_embeddings = out_embeddings[:k]
-    d = len(in_embeddings[0])
+    _, context_embeddings = read_embedding_file(context_embedding_file)
+    context_embeddings = context_embeddings[:k]
     in_word_idx = in_vocab.index(word_to_plot)
 
     # compute p(z | w)
     print "computing p(z | w)..."
     word_in_embedding = in_embeddings[in_word_idx]
-    p_z_w = compute_p_z_given_w(word_in_embedding, out_embeddings)
+    p_z_w = compute_p_z_given_w(word_in_embedding, context_embeddings)
 
     # NNs look best with dot on input-to-input embeddings
-    out_embeddings = in_embeddings
-
     # find nearest neighbors at the modes
-    sorted_prob_idx = np.argsort(-1*p_z_w) # negative one so the sort is descending
-    nns_at_modes = []
-    modes_used = []
-    idx = 0
-    while num_of_modes_to_plot > 0:
-        current_idx = sorted_prob_idx[idx]
-        # check if this idx is too close to previous ones
-        mode_flag = False
-        if (current_idx==0 and p_z_w[current_idx]>p_z_w[current_idx+1]) or (current_idx==d-1 and p_z_w[current_idx]>p_z_w[current_idx-1]) or (p_z_w[current_idx]>p_z_w[current_idx-1] and p_z_w[current_idx]>p_z_w[current_idx+1]): 
-            mode_flag = True
-            for mode in modes_used:
-                if abs(mode[0]-current_idx) <= 5:
-                    mode_flag = False
-        if mode_flag:
-            # get nearest neighbors at current idx
-            modes_used.append((current_idx, p_z_w[current_idx]))
-            nns_at_modes.append([out_vocab[j] for j in get_nearest_neighbors(word_in_embedding, in_word_idx, out_embeddings, current_idx+1, num_of_nns_to_get).tolist()])
-            num_of_modes_to_plot -= 1
-        idx += 1
-        if idx >= d:
-            break
-        
-    # plotting the distribution
-    plt.plot()
-    plt.bar([x+1 for x in range(d)], p_z_w, width=1.0, facecolor='blue', edgecolor="blue")
-    
-    # plot the nearest neighbors at the modes
-    for mode_loc, mode_nns in zip(modes_used, nns_at_modes):
-        plt.annotate(', '.join(mode_nns), xy=(mode_loc[0]+1, mode_loc[1]+0.001),  xycoords='data',
-                xytext=(mode_loc[0]+5, mode_loc[1]+0.005), arrowprops=dict(facecolor='black', shrink=0.05, frac=0.1, headwidth=2, width=1))
-    plt.title("p(z|w="+word_to_plot+")")
-    plt.xlim(1,d)
-    plt.ylim(0,modes_used[0][1]+0.007)
+    print "graphing..."
+    plt = graph_p_z_w(p_z_w, word_to_plot, in_word_idx, word_in_embedding, context_embeddings, in_vocab)  
     plt.savefig("p_z_w_"+word_to_plot+".png")
