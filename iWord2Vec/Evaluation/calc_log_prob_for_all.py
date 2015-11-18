5778960d584b8e79d4ccc750d0b8ef16c3856d93
enalisnick <enalisnick@gmail.com>
2015-11-18 14:34:31 -0800

    finished test_log_prob code, changed vectors to floats in eval_lib.h

--- iWord2Vec/Evaluation/calc_log_prob_for_all.py
+++ iWord2Vec/Evaluation/calc_log_prob_for_all.py
@@ -7,10 +7,11 @@ vecspath = "/Users/enalisnick/Desktop/vectors/"
 files = [ f for f in listdir(logspath) if isfile(join(logspath,f)) ]
 roots = []
 for file in files:
-    roots.append(file.split('PROGRESS_')[1].split('.log')[0])
+    if '0.05.log' in file: # only get the ones with higher learning rates
+        roots.append(file.split('PROGRESS_')[1].split('.log')[0])
 roots = set(roots)
 for root in roots:
     sparsity = float(root.split('_')[1])
     dim_penalty = float(root.split('_')[2])
     print "Calculating log probability for settings: %s" %(root)
-    os.system("./test_log_prob %s %s /Users/enalisnick/Desktop/word2vec/wiki_test_subset.txt %f %f" %(vecspath+'INPUT_VECS_'+root+'.txt', vecspath+'CONTEXT_VECS_'+root+'.txt', sparsity, dim_penalty))
+    os.system("./test_log_prob %s %s /Users/enalisnick/Desktop/word2vec/wiki_test_subset.txt %.8f %.8f" %(vecspath+'INPUT_VECS_'+root+'.txt', vecspath+'CONTEXT_VECS_'+root+'.txt', sparsity, dim_penalty))
